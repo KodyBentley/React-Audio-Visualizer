bf9bdc9945c6c900919379e8fa0e35b842d474a7
K Bentley <k.bentley@mrswordsmith.com>
2018-01-12 15:33:28 +0000

    Small typo fix in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ class Header extends React.Component<{}, {}> {
         return (
             <Jumbotron className="jumboHeader">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcometo React Audio Visualizer</h2>
+                <h2>Welcome to React Audio Visualizer</h2>
                 <p>Click play on controls to start with default visualization. Click different examples for different visualizations</p>
             </Jumbotron>
         );
